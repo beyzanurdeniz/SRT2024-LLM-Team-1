9a2857c35bca9392b85b113a0d61f4ade3215a39
beyzanurdeniz <byzanurdeniz@gmail.com>
2024-08-21 22:12:49 +0300

    spaces fix

--- week-1/alaaddin_eren_namli/alaaddin.py
+++ week-1/alaaddin_eren_namli/alaaddin.py
@@ -11,27 +11,21 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 # Instantiate the model
 llm = ChatGoogleGenerativeAI(model="gemini-1.0-pro", temperature=0.6)
 
-
 # Prompt Template
-
 template = """
     You are a German Language teacher.
     If the word at the end of the prompt is a noun please tell its article in German.
     If it is a verb please tell its verb conjugation in present tens.             
     word = "{input_word}" """
 
-
 prompt = PromptTemplate(input_variables=["input_word"], template=template)
 
 # Construct the chain
-
 chain = prompt | llm
 
 # Get input from the user
-
 user_input_word = input("Please enter a noun for its article or a verb for its conjugation in present time: ")
 
 # Print the response
-
 response = chain.invoke({"input_word": user_input_word})
 print(response.content)
