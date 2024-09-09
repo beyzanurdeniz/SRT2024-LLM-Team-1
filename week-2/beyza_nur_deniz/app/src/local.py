c9a06a29c98a75f6522adfb90f0bcd529529520a
beyzanurdeniz <byzanurdeniz@gmail.com>
2024-09-09 12:58:46 +0300

    application added for week-2 beyza

---
+++ week-2/beyza_nur_deniz/app/src/local.py
@@ -0,0 +1,19 @@
+gemini_api_key = "AIzaSyDvUCFyZPgmuYlQ3prHnw5BTHDI2tYVdDM"
+pinecone_api_key = "a9e00cf5-6c1f-4723-89b4-ede6038e62ba"
+
+pdf_files = [
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/cphb.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/01-intro.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/02-algorithms-1.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/03_math1.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/04_Graph1.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/05_DP1.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/06-data-structures-1.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/07_Graph2.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/08-data-structure-2.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/10_dp_2.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/11-graph-3.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/12_Math3.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/14-algorithms-5.pdf",
+    "/Users/beyzanurdeniz/Desktop/research-internship/SRT2024-LLM-Team-1/week-2/beyza_nur_deniz/app/data/inzva-bundles/Data-Structures-3.pdf"
+]
